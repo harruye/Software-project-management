240f178fb1f5a178a91f85d6157635a02c9a330e
harruye <1029536842@qq.com>
2023-03-05 01:49:10 +0800

    Try

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo1Application {
-
+    //dw
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
     }
